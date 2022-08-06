0bf432392fca3b9459c9e85375ef8cbf85fa5147
Nick <ntowle@gmail.com>
2022-08-06 13:16:45 -0400

    fix Karazhan boss list

--- ShadowlandsSeason4EncounterJournal.lua
+++ ShadowlandsSeason4EncounterJournal.lua
@@ -131,13 +131,13 @@ f:SetScript("OnEvent", function(self, event, addonName)
                         return nil
                     end
                 elseif selectedDungeon == "Lower" then
-                    if index > 4 then
+                    if index > 6 then
                         includedEncounterIDs[encounterID] = nil
                         return nil
                     end
                 elseif selectedDungeon == "Upper" then
-                    if index < 5 then
-                        index = index + 4
+                    if index < 7 then
+                        index = index + 6
                     else
                         includedEncounterIDs[encounterID] = nil
                         return nil
@@ -146,7 +146,9 @@ f:SetScript("OnEvent", function(self, event, addonName)
             end
             
             encounterID = select(3, oEJ_GetEncounterInfoByIndex(index, ...))
-            includedEncounterIDs[encounterID] = true
+            if encounterID then
+                includedEncounterIDs[encounterID] = true
+            end
             return oEJ_GetEncounterInfoByIndex(index, ...)
         end
         
