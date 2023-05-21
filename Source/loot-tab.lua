fb619907e3903bd8cb6411a7bdfabf4676069bff
Teelo <49792789+teelolws@users.noreply.github.com>
2023-05-22 09:03:38 +1200

    Fix addon blocked issue
    Closes https://github.com/teelolws/MplusAG/issues/14

--- Source/loot-tab.lua
+++ Source/loot-tab.lua
@@ -1,13 +1,13 @@
 local addonName = ...
 
 EventUtil.ContinueOnAddOnLoaded("Blizzard_EncounterJournal", function()
-    function EncounterJournal_CheckAndDisplayLootTab()
+    hooksecurefunc ("EncounterJournal_CheckAndDisplayLootTab", function()
         if (EJ_GetCurrentTier()==9) or (EJ_GetCurrentTier()==10) then 
             PanelTemplates_ShowTab(EncounterJournal, EncounterJournal.LootJournalTab:GetID()) 
         else 
             PanelTemplates_HideTab(EncounterJournal, EncounterJournal.LootJournalTab:GetID())
         end 
-    end
+    end)
     
     EncounterJournalLootJournalTab:HookScript("OnClick", 
         function() 
