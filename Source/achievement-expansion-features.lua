db532ac885f337848d12e8c1c0ff3eb7f8e73578
Teelo <49792789+teelolws@users.noreply.github.com>
2024-05-10 20:01:58 +1200

    Add "Remix: Pandaria" to achievement categories

--- Source/achievement-expansion-features.lua
+++ Source/achievement-expansion-features.lua
@@ -3,4 +3,5 @@ local addonName, addon = ...
 function addon:initAchievementExpansionFeatures()
     if not addon.db.profile.achievementExpansionFeatures then return end
     table.insert(ACHIEVEMENTUI_SUMMARYCATEGORIES, 15301)
+    table.insert(ACHIEVEMENTUI_SUMMARYCATEGORIES, 15509)
 end
