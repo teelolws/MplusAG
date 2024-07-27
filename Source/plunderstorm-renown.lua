9e6f4657b6e87845abe18d54f9ad673e4e3634b3
Teelo <49792789+teelolws@users.noreply.github.com>
2024-07-27 22:03:05 +1200

    Fix lua error

--- Source/plunderstorm-renown.lua
+++ Source/plunderstorm-renown.lua
@@ -4,7 +4,7 @@ local addonName, addon = ...
 
 function addon:initEnablePlunderstormRenownButton()
     hooksecurefunc(ReputationFrame.ReputationDetailFrame.ViewRenownButton, "Refresh", function(self)
-    	local factionID = C_Reputation.GetFactionDataByIndex(GetSelectedFaction()).factionID
+    	local factionID = C_Reputation.GetFactionDataByIndex(C_Reputation.GetSelectedFaction()).factionID
         if factionID == 2593 then
             self:Enable()
         end
